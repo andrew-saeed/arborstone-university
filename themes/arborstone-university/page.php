7cc0a5083307adb785aade1957e477c8eef9813b
andrew-saeed <andrew-saeed.dev@outlook.com>
2024-10-03 00:00:00 +0000

    create breadcrumb for any page has a parent-page

--- themes/arborstone-university/page.php
+++ themes/arborstone-university/page.php
@@ -1,7 +1,21 @@
 <? get_header(); ?>
 
+<? $pageParent = wp_get_post_parent_id(get_the_ID()); ?>
+
 <? while( have_posts() ): the_post(); ?>
-    <h1><? the_title(); ?></h1>
+
+    <header>
+        <h1><? the_title(); ?></h1>
+
+        <? if($pageParent): ?>
+            <nav aria-label="breadcrumb">
+                <ul class="flex gap-4">
+                    <li><a href="<? echo get_permalink($pageParent); ?>"><? echo get_the_title($pageParent); ?></a></li>
+                    <li aria-current="page"><? the_title(); ?></li>
+                </ul>
+            </nav>
+        <? endif; ?>
+    </header>
     <? the_content(); ?>
 <? endwhile; ?>
 
