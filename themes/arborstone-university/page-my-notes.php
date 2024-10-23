4ebaf4257bc68af9706125519fd71985c07f8d9c
andrew-saeed <andrew-saeed.dev@outlook.com>
2024-10-23 00:00:00 +0000

    notes update item

--- themes/arborstone-university/page-my-notes.php
+++ themes/arborstone-university/page-my-notes.php
@@ -25,13 +25,27 @@
                         while($myNotes->have_posts()): $myNotes->the_post(); 
                     ?>
                         <li data-id="<?= get_the_ID() ?>" x-data="note">
-                            <div class="note-box pb-20">
-                                <button class="btn btn--small btn--outline with-icon" @click="deleteItem">
+                            <div class="note-box">
+                                <button class="top-btns btn btn--small btn--outline with-icon" @click="deleteItem">
                                     <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M14 10V17M10 10V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                                     <span>delete</span>
                                 </button>
-                                <input name="title" id="title" type="text" value="<?= esc_attr(get_the_title()); ?>">
-                                <textarea name="content" id="content" rows="6"><?= esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                                <button class="top-btns btn btn--small btn--outline with-icon right-left-margin-1" @click="toggleEdit">
+                                    <svg x-show="!editMode" width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                                    <svg x-show="editMode" width="100%" height="100%" fill="#000000" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>cancel</title> <path d="M10.771 8.518c-1.144 0.215-2.83 2.171-2.086 2.915l4.573 4.571-4.573 4.571c-0.915 0.915 1.829 3.656 2.744 2.742l4.573-4.571 4.573 4.571c0.915 0.915 3.658-1.829 2.744-2.742l-4.573-4.571 4.573-4.571c0.915-0.915-1.829-3.656-2.744-2.742l-4.573 4.571-4.573-4.571c-0.173-0.171-0.394-0.223-0.657-0.173v0zM16 1c-8.285 0-15 6.716-15 15s6.715 15 15 15 15-6.716 15-15-6.715-15-15-15zM16 4.75c6.213 0 11.25 5.037 11.25 11.25s-5.037 11.25-11.25 11.25-11.25-5.037-11.25-11.25c0.001-6.213 5.037-11.25 11.25-11.25z"></path> </g></svg>
+                                    <span x-text="editMode? 'cancel' : 'edit'"></span>
+                                </button>
+                                <input readonly name="title" id="title" type="text" value="<?= esc_attr(get_the_title()); ?>">
+                                <textarea readonly name="content" id="content" rows="6"><?= esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                                <div class="footer-btns" x-cloak x-show="editMode">
+                                    <button class="btn btn--small btn--blue with-icon right-left-margin-1" @click="updateItem">
+                                        <svg x-show="processing" aria-hidden="true" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"/>
+                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"/>
+                                        </svg>
+                                        <span x-text="processing? 'updating' : 'update'"></span>
+                                    </button>
+                                </div>
                             </div>
                         </li>
                     <? endwhile; ?>
--- themes/arborstone-university/src/notesList.js
+++ themes/arborstone-university/src/notesList.js
@@ -3,26 +3,87 @@ import Alpine from "alpinejs"
 export default () => {
     Alpine.data('note', () => ({
         currentId: null,
+        itemInputText: null,
+        itemTextArea: null,
+        editMode: false,
+        currentTitle: '',
+        currentBody: '',
+        processing: false,
         init() {
             this.currentId = this.$el.dataset.id
-            this.$root.style.maxHeight = `${this.$root.scrollHeight}px`
+            this.$root.style.maxHeight = `${this.$root.scrollHeight + 80}px`
+
+            this.$nextTick(() => {
+                this.itemInputText = this.$root.querySelector('input')
+                this.itemTextArea = this.$root.querySelector('textarea')
+            })
         },
         async deleteItem() {
+            if(this.processing) return
+
             try {
-                await (await fetch(`http://localhost:10003/wp-json/wp/v2/note/${this.currentId}`, 
-                    {
-                        method: 'DELETE', 
-                        headers: {
-                            'X-WP-Nonce': document.body.dataset.nonce
-                        }
-                    })).json()
+                this.processing = true
+
+                await fetch(`http://localhost:10003/wp-json/wp/v2/note/${this.currentId}`, {
+                    method: 'DELETE', 
+                    headers: {
+                        'X-WP-Nonce': document.body.dataset.nonce
+                    }
+                })
+
                 this.$root.style.maxHeight = '0px'
+
+                this.processing = false
             } catch(err) {
                 console.log('error')
+                this.processing = false
             }
         },
-        animateItem() {
-            this.$root.style.maxHeight = '0px'
+        toggleEdit() {
+            if(this.processing) return
+
+            this.editMode = !this.editMode
+
+            if(this.editMode) {
+                this.currentTitle = this.itemInputText.value
+                this.currentBody = this.itemTextArea.value
+
+                this.itemInputText.removeAttribute('readonly')
+                this.itemTextArea.removeAttribute('readonly')
+            } else {
+                this.itemInputText.value = this.currentTitle
+                this.itemTextArea.value = this.currentBody
+
+                this.itemInputText.setAttribute('readonly', '')
+                this.itemTextArea.setAttribute('readonly', '')
+            }
+        },
+        async updateItem() {
+            if(this.processing) return
+
+            try {
+                this.processing = true
+
+                await fetch(`http://localhost:10003/wp-json/wp/v2/note/${this.currentId}`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        title: this.itemInputText.value,
+                        content: this.itemTextArea.value
+                    }),
+                    headers: {
+                        'X-WP-Nonce': document.body.dataset.nonce,
+                        'Content-Type': 'application/json'
+                    }
+                })
+
+                this.currentTitle = this.itemInputText.value 
+                this.currentBody = this.itemTextArea.value
+
+                this.processing = false
+            } catch(err) {
+                console.log('error')
+                this.processing = false
+            }
         }
     }));
 }
